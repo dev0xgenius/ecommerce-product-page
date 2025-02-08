82f173acb9b350096558bdfc10684f86e913d4b8
dev0xgenius <dev0xgenius@gmail.com>
2025-02-08 22:10:15 +0100

    Add nav bar functionality

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import Collapse from "bootstrap/js/dist/collapse.js";
+import Offcanvas from "bootstrap/js/dist/offcanvas.js";
+
+const collapseElementList = document.querySelectorAll('.collapse');
+const collapseList = [...collapseElementList].map(collapseEl => new Collapse(collapseEl));
+
+const offcanvasElementList = document.querySelectorAll('.offcanvas');
+const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new Offcanvas(offcanvasEl));
