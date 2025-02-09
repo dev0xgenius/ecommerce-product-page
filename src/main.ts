55190de0a8a40ff0caa97159098c2164b13de842
dev0xgenius <dev0xgenius@gmail.com>
2025-02-09 03:32:12 +0100

    Bootstrap JS: initialize carousel

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Collapse from "bootstrap/js/dist/collapse.js";
 import Offcanvas from "bootstrap/js/dist/offcanvas.js";
+import Carousel from "bootstrap/js/dist/carousel.js";
 
 const collapseElementList = document.querySelectorAll('.collapse');
 const collapseList = [...collapseElementList].map(collapseEl => new Collapse(collapseEl, {
@@ -8,3 +9,4 @@ const collapseList = [...collapseElementList].map(collapseEl => new Collapse(col
 
 const offcanvasElementList = document.querySelectorAll('.offcanvas');
 const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new Offcanvas(offcanvasEl));
+const carousel = new Carousel("#productDisplay");
