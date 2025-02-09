423f8a36d7fb6a53b4405a69615ca6c2cd85ea1e
dev0xgenius <dev0xgenius@gmail.com>
2025-02-09 02:51:41 +0100

    Customize nav, add cart card functionality and style

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import Collapse from "bootstrap/js/dist/collapse.js";
 import Offcanvas from "bootstrap/js/dist/offcanvas.js";
 
 const collapseElementList = document.querySelectorAll('.collapse');
-const collapseList = [...collapseElementList].map(collapseEl => new Collapse(collapseEl));
+const collapseList = [...collapseElementList].map(collapseEl => new Collapse(collapseEl, {
+  toggle: false,
+}));
 
 const offcanvasElementList = document.querySelectorAll('.offcanvas');
 const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new Offcanvas(offcanvasEl));
