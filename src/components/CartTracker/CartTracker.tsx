54faf938d4c68685518835dfd02f8d0fd4355e14
0xgenius <dev0xgenius@gmail.com>
2025-02-18 18:33:11 +0100

    Fix CartTracker height issue

--- src/components/CartTracker/CartTracker.tsx
+++ src/components/CartTracker/CartTracker.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
+import styles from './cartTracker.module.scss'
 
 export default function CartTracker() {
   return (
     <Container fluid className="p-2 h-100">
-      <Card className="shadow-lg rounded-4 border-0">
-        <Card.Header className="h6 bg-transparent p-4">Cart</Card.Header>
+      <Card className={`shadow-lg rounded-4 border-0 ${styles.tracker}`}>
+        <Card.Header className="h6 fw-bold bg-transparent p-4">Cart</Card.Header>
         <Card.Body className="d-flex justify-content-center text-center">
           <p className="card-text fw-bolder align-self-center text-secondary">
             Your cart is empty.
