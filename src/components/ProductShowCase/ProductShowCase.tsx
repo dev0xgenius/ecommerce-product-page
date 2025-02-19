8442ff7419cc81b57a1a60fffa7a65d5a8813c8b
0xgenius <dev0xgenius@gmail.com>
2025-02-19 15:16:26 +0100

    Customize and style carousel control icons

--- src/components/ProductShowCase/ProductShowCase.tsx
+++ src/components/ProductShowCase/ProductShowCase.tsx
@@ -1,27 +1,51 @@
 import React from 'react'
+import { useState } from 'react'
+import Container from 'react-bootstrap/Container'
 import Carousel from 'react-bootstrap/Carousel'
+import Button from 'react-bootstrap/Button'
+
+import styles from './productShowCase.module.scss'
 
 export default function ProductShowCase (){
+  const [index, setIndex] = useState(0);
+  
+  const prevIcon = ( 
+    <Button className={styles.carouselIcon}>
+      <img src="src/assets/images/icon-previous.svg" />
+    </Button>
+  );
+  
+  const nextIcon = (
+    <Button className={styles.carouselIcon}>
+      <img src="src/assets/images/icon-next.svg" />
+    </Button>
+  );
+  
   return (
-    <Carousel fade slide={true}>
+    <Carousel fade slide={true} activeIndex={index} 
+      onSelect={selectedIndex => setIndex(selectedIndex)}
+      prevIcon={prevIcon} nextIcon={nextIcon}
+    >
       <Carousel.Item>
-          <img src="src/assets/images/image-product-1.jpg" 
-            className="d-block w-100" alt="Sneaker image 1"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src="src/assets/images/image-product-2.jpg" 
-            className="d-block w-100" alt="Sneaker image 2"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src="src/assets/images/image-product-3.jpg" 
-            className="d-block w-100" alt="Sneaker image 3"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src="src/assets/images/image-product-4.jpg" className="d-block w-100" alt="Sneaker image 4" />
-        </Carousel.Item>
+        <img src="src/assets/images/image-product-1.jpg" 
+          className="d-block w-100" alt="Sneaker image 1"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="src/assets/images/image-product-2.jpg" 
+          className="d-block w-100" alt="Sneaker image 2"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="src/assets/images/image-product-3.jpg" 
+          className="d-block w-100" alt="Sneaker image 3"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="src/assets/images/image-product-4.jpg"
+          className="d-block w-100" alt="Sneaker image 4"
+        />
+      </Carousel.Item>
     </Carousel>
   )
 }
