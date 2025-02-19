0a3c34488851280b4d742cc5432672f7d7f33938
0xgenius <dev0xgenius@gmail.com>
2025-02-19 22:02:05 +0100

    Style Button and counter div with Bootstrap utilities

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 
+import styles from './counter.module.scss'
+
 export default function Counter() {
   return (
-    <div className="counter col-12 col-sm-4">
-      <Container fluid>
+    <div className={`col-12 col-sm-4 ${styles.counter} p-3 bg-light`}>
+      <Container fluid className="d-flex align-items-center justify-content-between">
         <Button variant="none">
           <img src="src/assets/images/icon-minus.svg" alt="minus symbol" />
         </Button>
-        <input type="number" min="0" value="0" />
+        <input type="number" min="0" value="0" 
+          className="d-inline-block w-100 text-center fw-bolder border-0 bg-transparent outline-none" 
+        />
         <Button variant="none">
           <img src="src/assets/images/icon-plus.svg" alt="plus symbol" />
         </Button>   
