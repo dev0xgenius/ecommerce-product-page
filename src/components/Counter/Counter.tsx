019abec51b59c66f24f26615bbbc6df449a4467c
0xgenius <dev0xgenius@gmail.com>
2025-02-19 22:22:28 +0100

    Add count state to Counter Component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+import { useState } from 'react'
 
 import styles from './counter.module.scss'
-
+  
 export default function Counter() {
+  const [count, setCount] = useState(0);
+  
   return (
     <div className={`col-12 col-sm-4 ${styles.counter} p-3 bg-light`}>
       <Container fluid className="d-flex align-items-center justify-content-between">
-        <Button variant="none">
+        <Button variant="none" onClick={() => (count != 0) && setCount(c => c - 1)}>
           <img src="src/assets/images/icon-minus.svg" alt="minus symbol" />
         </Button>
-        <input type="number" min="0" value="0" 
-          className="d-inline-block w-100 text-center fw-bolder border-0 bg-transparent outline-none" 
+        <input type="number" min={0} value={count} onChange={() => {}}
+          className="d-inline-block w-100 text-center fw-bolder border-0 bg-transparent" 
         />
-        <Button variant="none">
+        <Button variant="none" onClick={() => setCount(c => c + 1)}>
           <img src="src/assets/images/icon-plus.svg" alt="plus symbol" />
         </Button>   
       </Container>
