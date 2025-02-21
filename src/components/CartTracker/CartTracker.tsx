9122135de44a06a350f2577b85d571166ec3bb22
0xgenius <dev0xgenius@gmail.com>
2025-02-21 04:07:51 +0100

    Style Cart in CartTracker

--- src/components/CartTracker/CartTracker.tsx
+++ src/components/CartTracker/CartTracker.tsx
@@ -4,10 +4,9 @@ import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import styles from './cartTracker.module.scss'
 
 interface Props {
-  items: number,
+  items: double,
 };
 
 
@@ -16,33 +15,36 @@ export default function CartTracker({ items }:Props) {
     return (
       <>
         <Row xs={3} className="align-items-center justify-content-between gap-0">
-          <Col xs={2} as="span" className="col-2">
+          <Col xs={2} as="span">
             <img src="src/assets/images/image-product-1-thumbnail.jpg" 
               width='100%' className='rounded-2'
             />
           </Col>
-          <Col xs={8} className="p-2 d-flex flex-column">
-            <span>Fall Limited Edition Sneakers</span>
-            <span>$125.00 x {items} <b>${items * 125.00}</b></span>
+          <Col xs={8} className="p-2 d-flex flex-column align-items-start">
+            <span className="h6 fw-normal">Fall Limited Edition Sneakers</span>
+            <span>$125.00 x {items} <b>${125.00 * items}.00</b></span>
           </Col>
           <Col xs={1} as="span">
             <img src="src/assets/images/icon-delete.svg" />
           </Col>
         </Row> 
-        <Button className="p-3">Checkout</Button>
+        <Button className="p-3 fw-bold rounded-2">Checkout</Button>
       </>
     );
   }
   
   return (
     <Container fluid className="p-2 h-100">
-      <Card className={`shadow-lg rounded-4 border-0 ${styles.tracker}`}>
+      <Card className={`shadow-lg rounded-4 border-0`}>
         <Card.Header className="h6 fw-bold bg-transparent p-4">Cart</Card.Header>
-        <Card.Body className="d-flex flex-column justify-content-center text-center gap-2">
+        <Card.Body className="d-flex flex-column justify-content-center text-center gap-3 pt-4"
+        >
           {
             (items > 0) ? <Cart /> :
-            <Card.Text className="card-text fw-bolder justify-self-center text-secondary">
-              Your cart is empty.
+            <Card.Text className="fw-bolder d-flex text-secondary" style={{
+              minHeight: "100px", height: "100%"
+            }}>
+              <span className="align-self-center w-100 text-center">Your cart is empty.</span>
             </Card.Text>
           }
         </Card.Body>
